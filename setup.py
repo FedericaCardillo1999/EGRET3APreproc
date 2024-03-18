51cd53575c75fd7876f0273013fa47634f20ad9f
Federica Cardillo <163155072+FedericaCardillo1999@users.noreply.github.com>
2024-03-18 10:57:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,17 @@
-import os
 from setuptools import setup
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='EGRET3APreproc',
+   version='1.0',
+   description='Preprocessing anatomical and functional MRI data',
+   license="MIT",
+   long_description=long_description,
+   author='FedericaCardillo',
+   author_email='f.cardillo@umcg.nl',
+   url="https://github.com/FedericaCardillo1999/EGRET3APreproc",
+   packages=['EGRET3APreproc'],  #same as name
+   install_requires=['wheel', 'bar', 'greek']
+)
